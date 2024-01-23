65ca2c83a5ec387102eb020a971fcef1aba9009d
stravo1@msi <stravoone@gmail.com>
2024-01-23 14:38:20 +0530

    chore: combined all branches
    home: added back loading anim
    about: prep for actual data
    contact: created the page
    custom-tags/modal: added
    custom-tags/page-transition: added desktop menu
    events: added new card and made content generation dynamic, also added modal

--- custom-tags/page-transition-animation/script.js
+++ custom-tags/page-transition-animation/script.js
@@ -1,33 +1,84 @@
-const transitionWrapper = document.getElementsByClassName("transition-wrapper")[0];
-var routesLinks = document.getElementsByClassName("routes-mobile")[0].childNodes
+const transitionWrapper =
+  document.getElementsByClassName("transition-wrapper")[0];
+var routesLinks =
+  document.getElementsByClassName("routes-mobile")[0].childNodes;
+var routesLinksDesktopGroup1 = document.querySelector(
+  ".desktop .nav-group-1"
+).childNodes;
+var routesLinksDesktopGroup2 = document.querySelector(
+  ".desktop .nav-group-2"
+).childNodes;
 
 routesLinks.forEach((node) => {
-    node.addEventListener("click", (e) => {
-        e.stopPropagation();
-        e.preventDefault();
-        if (`/${node.id}/` == path) {
-            closeMenu();
-            return;
-        } else if ((node.id == "home") && (path == "/")) {
-            closeMenu();
-            return;
-        }
-        localStorage.setItem("playTransitionAnimation", true)
-        transitionWrapper.classList.remove("transition-invisible");
-        transitionWrapper.classList.add("transition-visible");
-        setTimeout(() => {
-            window.location.replace(`/${node.id == "home" ? "" : node.id}`);
-        }, 250)
-    })
-})
+  node.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    if (`/${node.id}/` == path) {
+      closeMenu();
+      return;
+    } else if (node.id == "home" && path == "/") {
+      closeMenu();
+      return;
+    }
+    localStorage.setItem("playTransitionAnimation", true);
+    transitionWrapper.classList.remove("transition-invisible");
+    transitionWrapper.classList.add("transition-visible");
+    setTimeout(() => {
+      window.location.replace(`/${node.id == "home" ? "" : node.id}`);
+    }, 250);
+  });
+});
 
+routesLinksDesktopGroup1.forEach((node) => {
+  node.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    let nodePath = node.id.split("-")[0];
+    console.log(nodePath);
+    // return
+    if (`/${nodePath}/` == path) {
+      closeMenu();
+      return;
+    } else if (nodePath == "home" && path == "/") {
+      closeMenu();
+      return;
+    }
+    localStorage.setItem("playTransitionAnimation", true);
+    transitionWrapper.classList.remove("transition-invisible");
+    transitionWrapper.classList.add("transition-visible");
+    setTimeout(() => {
+      window.location.replace(`/${nodePath == "home" ? "" : nodePath}`);
+    }, 250);
+  });
+});
+
+routesLinksDesktopGroup2.forEach((node) => {
+  node.addEventListener("click", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    let nodePath = node.id.split("-")[0];
+    if (`/${nodePath}/` == path) {
+      closeMenu();
+      return;
+    } else if (nodePath == "home" && path == "/") {
+      closeMenu();
+      return;
+    }
+    localStorage.setItem("playTransitionAnimation", true);
+    transitionWrapper.classList.remove("transition-invisible");
+    transitionWrapper.classList.add("transition-visible");
+    setTimeout(() => {
+      window.location.replace(`/${nodePath == "home" ? "" : nodePath}`);
+    }, 250);
+  });
+});
 
 if (localStorage.getItem("playTransitionAnimation")) {
-    localStorage.removeItem("playTransitionAnimation");
-    setTimeout(() => {
-        transitionWrapper.classList.add("transition-invisible");
-        transitionWrapper.classList.remove("transition-visible");
-    }, 250)
+  localStorage.removeItem("playTransitionAnimation");
+  setTimeout(() => {
+    transitionWrapper.classList.add("transition-invisible");
+    transitionWrapper.classList.remove("transition-visible");
+  }, 250);
 } else {
-    transitionWrapper.classList.add("transition-invisible-no-anim");
+  transitionWrapper.classList.add("transition-invisible-no-anim");
 }
--- events/script.js
+++ events/script.js
@@ -1,36 +1,88 @@
 // Code scrolling effects of the page header:
+let header = document.querySelector("#page-header");
+let msg = document.querySelector("#page-msg");
+let topSection = document.querySelector(".top-section");
+let sponsorSection = this.document.querySelector(".events-items");
+let initialTopPaddingForMsg = "10px";
+
+const resetPositions = () => {
+  header.style.transition = "0.1s font-size, 0.3s padding, top 0.3s linear";
+  header.style.fontSize = "2.5rem";
+  header.style.position = "sticky";
+  header.style.paddingLeft = "40px";
+  msg.style.opacity = "1";
+  msg.style.paddingTop = initialTopPaddingForMsg;
+  topSection.style.height = "45%";
+  topSection.style.minHeight = "300px";
+  sponsorSection.style.paddingTop = "70px";
+};
+
 window.addEventListener("scroll", function () {
-    let header = document.querySelector("#page-header");
-    let msg = document.querySelector("#page-msg");
-    let topSection = document.querySelector(".top-section");
-    let sponsorSection = this.document.querySelector(".sponsor-items");
-    let currentScrollPos = window.pageYOffset;
-    console.log(currentScrollPos)
-    if (currentScrollPos > 10) {
-        header.style.paddingLeft = "90px";
-        header.style.transition = "0.1s font-size, 0.1s padding, top 0.3s linear, 0.3s box-shadow";
-        topSection.style.minHeight = "80px";
-        if (currentScrollPos > 50)
-            header.style.fontSize = "1.8rem";
-        if (currentScrollPos > 100) {
-            msg.style.opacity = "0";
-            msg.style.left = "60px";
-            header.style.position = "fixed";
-            sponsorSection.style.paddingTop = "120px";
-            topSection.style.height = "80px";
-        }
-        if (currentScrollPos > 118)
-            header.style.boxShadow = "0 25px 30px black";
+  let msgPositionFromTop = msg.getBoundingClientRect().top;
+  console.log(msgPositionFromTop);
+  if (msg.style.paddingTop == "65px") {
+    if (msgPositionFromTop > 50) {
+      resetPositions();
     }
-    else {
-        header.style.transition = "0.1s font-size, 0.3s padding, top 0.3s linear, 0s box-shadow";
-        header.style.fontSize = "2.5rem";
-        header.style.position = "sticky";
-        header.style.paddingLeft = "40px";
-        header.style.boxShadow = "0 0 0 black";
-        msg.style.opacity = "1";
-        topSection.style.height = "45%";
-        topSection.style.minHeight = "300px";
-        sponsorSection.style.paddingTop = "70px";
+  }
+  if (msgPositionFromTop < 110) {
+    header.style.paddingLeft = "90px";
+    header.style.transition =
+      "0.1s font-size, 0.1s padding, top 0.3s linear, 0.3s box-shadow";
+    if (msgPositionFromTop < 85) {
+      header.style.fontSize = "1.8rem";
+      msg.style.transition = "none";
     }
+    if (msgPositionFromTop <= 0) {
+      msg.style.paddingTop = "65px";
+      header.style.position = "fixed";
+    }
+  } else {
+    resetPositions();
+  }
+  setTimeout(() => {
+    let msgPositionFromTop = msg.getBoundingClientRect().top;
+    console.log(msgPositionFromTop);
+    if (msgPositionFromTop > 110) {
+      resetPositions();
+    }
+  }, 100);
+});
+
+const eventDetails = [
+  {
+    name: "Codeplay",
+    description:
+      "Experience the mesmerizing spectacle of diverse rhythms colliding on one stage in Xavotsav.",
+    modalDescription:
+      "Immerse in the rhythmic world of dance at Xavotsav 2024, where movement reigns supreme, stirring hearts and sparking expressive passion. This vibrant genre promises a kaleidoscope of performances, showcasing the dynamic diversity within the dance community.<br><br>Join us in celebrating the artistry, dedication, and sheer brilliance that dance brings to life at Xavotsav 2024. <ul><li>Eastern solo</li><li>Bollywood Dance</li><li>Street Battle</li><li>Western Duet</li><li>Western Group</li></ul>",
+    formLink: "https://google.com",
+    icon: "data_object",
+  },
+];
+
+let eventSection = document.querySelector(".events-items");
+let eventSectionDesktop = document.querySelector(".event-items-desktop");
+
+eventDetails.forEach((event) => {
+  let newEventDivDeskop = document.createElement("div");
+  newEventDivDeskop.className = "event-card white-bg rounded";
+  newEventDivDeskop.innerHTML = `
+              <div class="card-top flex justify-between p-1rem">
+                <div class="pixelated text-lg">${event.name}</div>
+                <div
+                  class="card-symbol flex justify-center items-center circle"
+                >
+                  <span class="material-symbols-outlined"> ${event.icon} </span>
+                </div>
+              </div>
+              <div class="card-content p-1rem text-sm font-bold">
+                ${event.description}
+              </div>
+              <div class="card-button p-1rem flex justify-between gap-1rem">
+                <button class="filled cursor-pointer" disabled onclick="openModal('${event.name}','${event.modalDescription}','${event.formLink}')">Registration Not Yet Open</button>
+              </div>
+  `;
+  eventSectionDesktop.appendChild(newEventDivDeskop);
+  eventSection.appendChild(newEventDivDeskop);
 });
